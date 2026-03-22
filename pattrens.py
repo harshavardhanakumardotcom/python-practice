3d4df207f600e8eecf2e093e3b7a9f7407c32d1c
harshavardhanakumardotcom <gottipatiharshavardhan107@gmail.com>
2026-03-22 09:26:42 +0530

    solved  pattrens

--- pattrens.py
+++ pattrens.py
@@ -13,12 +13,54 @@ for i in range (1,n+1):
     for j in range (n-i):
         print(" ",end="")
     for k in range(i):
-        print("*",end=" ")
+        print(k,end=" ")
     print()
 
 for i in range (n,0,-1):
     for j in range (n-i):
         print(" ",end="")
     for k in range (i):
-        print("*",end=" ")
+        print(k,end=" ")
+    print()
+
+for i in range (1,n+1):
+    for j in range(n-i):
+        print(" ",end ="")
+    for k in range(1,i+1):
+        print(k,end=" ")
+    print()
+
+    #FLOYD'S TRIANGLE
+
+print("floyd's triangle")
+num = 1
+
+for i in range (1,n+1):
+    for j in range(i):
+        print(num,end=" ")
+        num = num+1
+    print()
+sum=1
+for i in range (1,n+1):
+    for j in range(i):
+        print(sum,end=" ")
+        sum+=2
+    print()
+
+print("centered number pyramid")
+for i in range (1,n+1):
+    for j in range(n-i):
+        print("  ",end="")
+    for j in range(1,i+1):
+        print(j,end=" ")
+    for j in range(i-1,0,-1):
+        print(j,end=" ")
+    print()
+for i in range(n,0,-1):
+    for j in range(n-i):
+        print("  ",end="")
+    for j in range(1,i+1):
+        print(j,end=" ")
+    for j in range(i-1,0,-1):
+        print(j,end=" ")
     print()
